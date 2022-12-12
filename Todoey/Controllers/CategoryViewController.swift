67f407fc4610f6cb50ccf1223463d970e3d59c17
Rosalyn Land <roz.s.land@gmail.com>
2022-12-12 09:52:06 +0000

    Show alert to add new categories to array

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -11,6 +11,9 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,7 +21,30 @@ class CategoryViewController: UITableViewController {
 
     // MARK: - Add New Categories
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Add Category", message: nil, preferredStyle: .alert)
 
+        alertController.addTextField { textField in
+            textField.placeholder = "Create a new category"
+        }
+
+        let addCategoryAction = UIAlertAction(title: "Add", style: .default) { [weak alertController] _ in
+            guard let textFields = alertController?.textFields else { return }
+            if let categoryText = textFields.first?.text {
+
+                let newCategory = Category(context: self.context)
+                newCategory.name = categoryText
+                self.addCategory(newCategory)
+            }
+        }
+
+        alertController.addAction(addCategoryAction)
+
+        present(alertController, animated: true)
+    }
+
+    private func addCategory(_ category: Category) {
+        categories.append(category)
+        print(categories)
     }
 
     // MARK: - TV Datasource Methods
